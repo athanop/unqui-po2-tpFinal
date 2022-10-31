b237d7979a68f343c80ffa6d853c2429099222bf
ivanapr <vanndere@gmail.com>
2022-10-30 23:45:54 -0300

    Implementacion filtro por texto

--- src/ar/edu/unq/po2/BusquedaProyecto/FiltroPorTexto.java
+++ src/ar/edu/unq/po2/BusquedaProyecto/FiltroPorTexto.java
@@ -1,15 +1,21 @@
 package ar.edu.unq.po2.BusquedaProyecto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ar.edu.unq.po2.Proyecto;
 
+/**
+ * Clase que modela la busqueda de proyectos con el filtro de texto en titulos.
+ */
 public class FiltroPorTexto implements BusquedaComponent {
-
+	private String texto;
+	public FiltroPorTexto(String text) {
+		this.texto = text;
+	}
 	@Override
 	public List<Proyecto> filtrados(List<Proyecto> proyectos) {
-		// TODO Auto-generated method stub
-		return null;
+		return proyectos.stream().filter(p -> p.getNombre().contains(this.texto)).collect(Collectors.toList());
 	}
 
 }
