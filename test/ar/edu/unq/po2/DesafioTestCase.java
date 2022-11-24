c076ea339960f6e174cf6538722b7688eaebdd80
ivanapr <vanndere@gmail.com>
2022-11-24 02:29:05 -0300

    Se completa test de Desafio

--- test/ar/edu/unq/po2/DesafioTestCase.java
+++ test/ar/edu/unq/po2/DesafioTestCase.java
@@ -1,12 +1,17 @@
 package ar.edu.unq.po2;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.po2.Area.Area;
+import ar.edu.unq.po2.Area.Coordenada;
 import ar.edu.unq.po2.RestriccionTemporal.RestriccionTemporal;
 
 class DesafioTestCase {
@@ -14,12 +19,15 @@ class DesafioTestCase {
 	private Desafio desafio;
 	private RestriccionTemporal restriccion;
 	private Area area;
+	private Coordenada coord;
+	private LocalDate fecha;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		restriccion = mock(RestriccionTemporal.class);
 		area = mock(Area.class);
 		desafio = new Desafio(1, 2, 50, restriccion, area);
+		coord = mock(Coordenada.class);
 	}
 
 	@Test
@@ -47,5 +55,17 @@ class DesafioTestCase {
 	@Test
 	void testArea() {
 		assertEquals(desafio.getArea(), area);
+	}
+	
+	@Test
+	void testCoordenadaCumpleAreaDesafio() {
+		when(desafio.getArea().coordenadaEstaDentroDelArea(coord)).thenReturn(true);
+		assertTrue(desafio.coordenadaCumpleAreaDesafio(coord));
+	}
+	
+	@Test
+	void testFechaCumpleRestriccionTemporal() {
+		when(desafio.getRestriccion().esFechaPermitida(fecha)).thenReturn(true);
+		assertTrue(desafio.fechaCumpleRestriccionTemporal(fecha));
 	}
 }
